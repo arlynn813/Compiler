ca01745463c43e79d8af1695dbab6b90133bdfea
arlynn813 <lynn12@purdue.edu>
2019-11-26 21:17:36 -0500

    fixed bug in pushv

--- src/statement/manipulation/Pushv.java
+++ src/statement/manipulation/Pushv.java
@@ -8,16 +8,16 @@ public class Pushv extends Statement {
     private byte pushi;
 
     public Pushv() {
-        pushi = (byte) 71;
+        pushi = (byte) 70;
         pushvi = (byte) 74;
     }
 
     public byte[] generateOpcodes(String[] tokens) {
         byte[] opcodes = new byte[1];
-        int offset = symbol_table.get("main" + tokens[1]) - 1;  // -1 is hacky solution to offset problem
+        byte[] location = integerToBytes(symbol_table.get("main" + tokens[1]) - 1);  // -1 is hacky solution to offset problem
 
         opcodes[0] = pushi;
-        opcodes = append(opcodes, integerToBytes(offset));
+        opcodes = append(opcodes, location);
         opcodes = append(opcodes, pushvi);
 
         return opcodes;
