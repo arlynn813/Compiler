0ed62cbd9faba6db2551b82f18c71b9c7a75b89c
arlynn813 <lynn12@purdue.edu>
2019-11-26 19:04:58 -0500

    adding var statements to opcode

--- src/statement/Opcode.java
+++ src/statement/Opcode.java
@@ -14,9 +14,10 @@ public class Opcode {
 
     // Make sure statementObjects and statementStrings are in the correct order for mapping!
     private static Statement[] statementObjects = {new Printi(), new Pushi(), new Cmpe(), new Cmplt(), new Cmpgt(),
-            new Swp(), new Add(), new Sub(), new Mul(), new Div(), new Popm(), new Decl(), new Subr()};
+            new Swp(), new Add(), new Sub(), new Mul(), new Div(), new Popm(), new Decl(), new Subr(), new Printv(),
+            new Popv(), new Pushv()};
     private static String[] statementStrings = {"printi", "pushi", "cmpe", "cmplt", "cmpgt", "swp", "add", "sub",
-            "mul", "div", "popm", "decl", "subr"};
+            "mul", "div", "popm", "decl", "subr", "printv", "popv", "pushv"};
     private static Map<String, Statement> statementMap = new HashMap<>();
 
     public Opcode() {
@@ -32,7 +33,7 @@ public class Opcode {
         }
 
         // TODO: If we decide to still return null here, we must raise an error from within the compile method
-        System.out.println("Statement not found. Make sure the statement is contained in statementStrings array.");
+        System.out.println("Statement: " + tokens[0] + " not found. Make sure the statement is contained in statementStrings array.");
         return null;
     }
 }
