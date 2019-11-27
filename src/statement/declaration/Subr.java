2f29fa8013e884a9c114a24782577cf76af28158
arlynn813 <lynn12@purdue.edu>
2019-11-26 22:36:38 -0500

    fixed subr 0 main to follow instructor usage

--- src/statement/declaration/Subr.java
+++ src/statement/declaration/Subr.java
@@ -4,11 +4,11 @@ import statement.Statement;
 
 
 public class Subr extends Statement {
-    private byte call;
+    private byte[] call;
     private byte pushi;
 
     public Subr() {
-        call = (byte) 44;
+        call = new byte[] {(byte) 44, (byte) 0};
         pushi = (byte) 70;
     }
 
@@ -16,12 +16,19 @@ public class Subr extends Statement {
     // This statement will only/always be found at the first line of the input file
     public byte[] generateOpcodes(String[] tokens) {
 
-        // Currently hard coding call instruction as call is only used for the initial main subroutine
+        // Instructor steps:
+        // pushi 16
+        // pushi 17
+        // pushi 1
+        // call
+
         byte[] opcodes = new byte[1];
         opcodes[0] = pushi;
-        opcodes = append(opcodes, integerToBytes(11));  // location of next instruction after subr and call
+        opcodes = append(opcodes, integerToBytes(16));
         opcodes = append(opcodes, pushi);
-        opcodes = append(opcodes, integerToBytes(0));  // offset of main subroutine
+        opcodes = append(opcodes, integerToBytes(17));
+        opcodes = append(opcodes, pushi);
+        opcodes = append(opcodes, integerToBytes(1));
         opcodes = append(opcodes, call);
 
         return opcodes;
