9f057ba5a438f68d9f3cff4e0c88605461a4e067
arlynn813 <lynn12@purdue.edu>
2019-11-27 13:26:12 -0500

    decl cleanup

--- src/statement/declaration/Decl.java
+++ src/statement/declaration/Decl.java
@@ -6,15 +6,10 @@ import statement.manipulation.Pushi;
 
 
 public class Decl extends Statement {
-    String Var; //variable name
 
-
-    private String getkey(String key, String func_label) {
-        return key+func_label;
-    }
     public byte[] generateOpcodes(String[] tokens) {
         Rstack value = new Rstack();
-        symbol_table.put(getkey("main",tokens[1]),value.get_offset());
+        symbol_table.put("main" + tokens[1],value.get_offset());
         Pushi out_obj = new Pushi();
         String[] arr = {"pushi", "0"};
         return out_obj.generateOpcodes(arr);
