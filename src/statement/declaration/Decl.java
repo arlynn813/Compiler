026ca5db852bc450ab46eb97bdbb99b7642eae0b
arlynn813 <lynn12@purdue.edu>
2019-11-28 23:50:37 -0500

    fixed bug in declared vars after label read

--- src/statement/declaration/Decl.java
+++ src/statement/declaration/Decl.java
@@ -8,8 +8,11 @@ import statement.manipulation.Pushi;
 public class Decl extends Statement {
 
     public byte[] generateOpcodes(String[] tokens) {
-        Rstack value = new Rstack();
-        symbol_table.put("main" + tokens[1],value.get_offset());
+        if (!symbol_table.containsKey("main" + tokens[1])) {
+            Rstack value = new Rstack();
+            symbol_table.put("main" + tokens[1], value.get_offset());
+        }
+
         Pushi out_obj = new Pushi();
         String[] arr = {"pushi", "0"};
         return out_obj.generateOpcodes(arr);
